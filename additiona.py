054d7db6ca9a862c11e48550113af583ac36dc84
varadavaidya <varadavaidya07@gmail.com>
2024-07-05 16:47:07 +0530

    sth

---
+++ additiona.py
@@ -0,0 +1,9 @@
+def add(x,y):
+    sum=x**2+y**2
+    print(sum)
+
+def multiply(x,y):
+    if __name__ == '__main__': #only run in this .py file
+        # or else it wont run when imported in another file#
+        multiply=x*y
+        print(multiply)
